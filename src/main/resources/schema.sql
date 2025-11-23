59ca0f2e5a01264ea97e139bc7e2bce3dfe3dec4
ahmed <ahmedjshlibek@gmail.com>
2025-11-23 17:44:25 +0200

    feat: Implement core ERP database schema and normalization
    - Added five essential tables: users, categories, products, orders, and order_items.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id BINARY(16) primary key,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) not null,
+    role VARCHAR(25) not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS categories (
+    category_id BINARY(16) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS products (
+    product_id BINARY(16) primary key,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    category_id BINARY(16) NOT NULL,
+    price BIGINT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+        REFERENCES categories(category_id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id BINARY(16) primary key,
+    user_id BINARY(16) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS order_items (
+    order_id BINARY(16) NOT NULL,
+    product_id BINARY(16) NOT NULL,
+    PRIMARY KEY (order_id, product_id),
+    price_at_order BIGINT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_order_header
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_order_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
